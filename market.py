53d21e55023fd288607701f3aef8d8c9f5a33ecf
bgcarmin <bgcarmin.ab@gmail.com>
2022-11-29 12:05:16 +0100

    Dodan homepage

--- market.py
+++ market.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
+    
 
-
-@app.route("/about/<username>")
-def about_page(username):
-    return f'<h1>This is about page of {username}</h1>'
