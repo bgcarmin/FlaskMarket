64f789ae970301bd397c4983885981b76135dd56
bgcarmin <bgcarmin.ab@gmail.com>
2022-11-29 11:31:50 +0100

    Test rute

--- market.py
+++ market.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/about/<username>")
+def about_page(username):
+    return f'<h1>This is about page of {username}</h1>'
