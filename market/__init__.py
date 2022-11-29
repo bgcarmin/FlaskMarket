26218dcc4df511ea622f6086ed9edfc7e7a24047
bgcarmin <bgcarmin.ab@gmail.com>
2022-11-29 17:16:51 +0100

    Dodana flask forma za registraciju

--- market/__init__.py
+++ market/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'cb9815a3ef8e2ebaeac962d3'
 db = SQLAlchemy(app)
 
 from market import routes
