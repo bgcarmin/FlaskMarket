d33e8ed6f3f983331dcd79625a1377ce76c2f9ae
bgcarmin <bgcarmin.ab@gmail.com>
2022-11-30 16:04:54 +0100

    Implementirana funkcionalnost kupovine proizvoda za prijavljene korisnike

--- market/forms.py
+++ market/forms.py
@@ -26,3 +26,10 @@ class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
     password = PasswordField(label='Password:', validators=[DataRequired()])
     submit = SubmitField(label='Sign In')
+
+
+class PurchaseItemForm(FlaskForm):
+    submit = SubmitField(label='Purchase item!')
+
+class SellItemForm(FlaskForm):
+    submit = SubmitField(label='Sell item!')
